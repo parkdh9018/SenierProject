7c4dcd9407e18dabc270ece74aa3c2685ae1a9ea
enirobot <enirobot@naver.com>
2018-01-10 18:07:09 +0900

    성능 개선
    하지만 프로그램을 종료하고 바로 시작하면 중복되어 값이 들어갈 수 있음.

--- Web_scraping/test_Web_scraping.py
+++ Web_scraping/test_Web_scraping.py
@@ -1,3 +1,4 @@
+"""
 import csv
 from operator import eq
 from urllib.request import urlopen
@@ -52,51 +53,40 @@ try:
                 break
 finally:
     csvFile.close()
-
-
 """
+
+
 # 스레드만 추가, 성능개선 필요
 import threading
+import copy
 import csv
-from operator import eq
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+compareList = []
 
 class AsyncTask:
-    global i
-
     def __init__(self):
-        i = 0
+        try:
+            csvFile = open('news.csv', 'r', encoding='utf-8')
+            csvFile.close()
+        except:
+            csvFile = open('news.csv', 'w', encoding='utf-8')
+            writer = csv.writer(csvFile)
+            writer.writerow(('Title', 'Media', 'Date'))
+            csvFile.close()
         pass
 
 
     def TaskA(self):
+        global compareList
+        tempList = []
+
         html = urlopen("http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001")
         bsObj = BeautifulSoup(html, "html.parser")
 
         table = bsObj.findAll("div", {"class": "list_body newsflash_body"})[0]
         rows = table.findAll("li")
-
-        compareList = []
-
-        try:
-            i = 0
-            csvFile = open('news.csv', 'r')
-            rdr = csv.reader(csvFile)
-
-            for line in rdr:
-                if i >= 20:
-                    break
-                compareList.append(line[0])
-                i += 1
-
-            csvFile.close()
-        except:
-            csvFile = open('news.csv', 'w')
-            writer = csv.writer(csvFile)
-            writer.writerow(('Title', 'Media', 'Date'))
-            csvFile.close()
 
         csvFile = open("news.csv", 'a')
         writer = csv.writer(csvFile)
@@ -113,18 +103,26 @@ class AsyncTask:
                 media = temp.find("span", {"class": "writing"}).get_text()
                 date = temp.find("span", {"class": "date"}).get_text()
 
-                for item in compareList:
-                    if not eq(item, title):
-                        writer.writerow((title, media, date))
-                        break
+                tempList.append(title)
+
+                if compareList.count(title) == 0:
+                    writer.writerow((title, media, date))
+            print("scraping")
+
+            print("compareList")
+            print(compareList)
+            print("tempList")
+            print(tempList)
+
+            compareList.clear()
+            compareList = copy.deepcopy(tempList)
+
         except:
-            print("못찾음")
+            print("예외발생")
         finally:
             csvFile.close()
-        i += 1
-        print("scraping : " + str(i))
 
-        threading.Timer(60,self.TaskA).start()
+        threading.Timer(10,self.TaskA).start()
 
 
 def main():
@@ -133,4 +131,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-"""
+
