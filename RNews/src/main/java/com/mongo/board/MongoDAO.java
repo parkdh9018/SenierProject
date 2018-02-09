02b3b53302e8979c980bc778d0603d8b0306be23
parkdh9018 <parkdh9018@naver.com>
2018-02-09 14:00:40 +0900

    Update MongoDAO.java

--- RNews/src/main/java/com/mongo/board/MongoDAO.java
+++ RNews/src/main/java/com/mongo/board/MongoDAO.java
@@ -10,6 +10,8 @@ import org.springframework.data.mongodb.core.query.*;
 
 import com.mongodb.MongoClient;
 
+
+//MVC 증 Model을 담당
 public class MongoDAO {
 	
 	 MongoOperations mongoOps = new MongoTemplate(new SimpleMongoDbFactory(new MongoClient(), "project"));
@@ -19,15 +21,18 @@ public class MongoDAO {
 	
 	public MongoDAO() {}
 	
+	//데이터베이스에 삽입
 	public void insert(News news) {
 		mongoOps.insert(news);
 		System.out.println("success : " + mongoOps);
 	}
+	//collection내의 모든 raw를 List에 저장
 	public List<News> findAll() {
 		List<News> news = mongoOps.findAll(News.class);		
 		return news;
 	}
 	
+	//where의 조건이 value에 해당하는 raw를 list에 저장
 	public List<News> find(String where, String value){
 		Query query = new Query();
 		query.addCriteria(Criteria.where(where).is(value));
@@ -36,6 +41,7 @@ public class MongoDAO {
 		return list;
 	}
 	
+    //
     public void update(News news) {
     	mongoOps.save(news);
     }
